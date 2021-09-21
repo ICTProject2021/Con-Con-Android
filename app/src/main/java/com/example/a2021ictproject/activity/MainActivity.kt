fecebe8264a5b65d8bdda368531f406d1a2bfc44
tmddusqls <tmddusqls@naver.com>
2021-09-22 08:59:06 +0900

    .

--- app/src/main/java/com/example/a2021ictproject/activity/MainActivity.kt
+++ app/src/main/java/com/example/a2021ictproject/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.a2021ictproject
+package com.example.a2021ictproject.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a2021ictproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
