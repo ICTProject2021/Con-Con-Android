b8fd457a65eb7a807bd22ce01f820144cb626f99
지민혁 <63153516+MinHyukJi1226@users.noreply.github.com>
2021-09-27 10:47:12 +0900

    [ADD] Bottom Appbar 생성

--- app/src/main/java/com/example/a2021ictproject/activity/MainActivity.kt
+++ app/src/main/java/com/example/a2021ictproject/activity/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.a2021ictproject.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.a2021ictproject.R
+import com.example.a2021ictproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 }
