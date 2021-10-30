7198c33ee9103afc6ac2e1369e8337c4ef2b6fc4
tmddusqls <tmddusqls@naver.com>
2021-10-30 15:20:27 +0900

    update: Intro Button selector 설정

--- app/src/main/java/com/example/a2021ictproject/fragment/IntroFragment.kt
+++ app/src/main/java/com/example/a2021ictproject/fragment/IntroFragment.kt
@@ -1,12 +1,10 @@
 package com.example.a2021ictproject.fragment
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.a2021ictproject.R
