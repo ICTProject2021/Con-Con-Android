e67e2b764818d436bc05408bb3fe2677714341ae
tmddusqls <tmddusqls@naver.com>
2021-11-14 20:19:19 +0900

    rename: Rename DialogViewModel to PrizeDialogViewModel

--- app/src/main/java/com/project/concon/viewmodel/PrizeDialogViewModel.kt
+++ app/src/main/java/com/project/concon/viewmodel/PrizeDialogViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.project.concon.network.dto.response.Prize
 
-class DialogViewModel : ViewModel() {
+class PrizeDialogViewModel : ViewModel() {
 
     var prize = MutableLiveData<Int>()
     var prizeList = MutableLiveData<List<Prize>>(listOf())
