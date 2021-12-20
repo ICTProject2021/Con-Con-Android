ee584457fff369b7bd66d23f3b674321090cd619
tmddusqls <tmddusqls@naver.com>
2021-12-20 12:15:38 +0900

    refactor: retrofit service 파일 rxJava 형식으로 변경

--- app/src/main/java/com/project/concon/model/remote/dao/AccountService.kt
+++ app/src/main/java/com/project/concon/model/remote/dao/AccountService.kt
@@ -1,34 +1,36 @@
 package com.project.concon.model.remote.dao
 
+import com.project.concon.model.remote.dto.Res
+import com.project.concon.model.remote.dto.request.CashRequest
 import com.project.concon.model.remote.dto.request.IdRequest
 import com.project.concon.model.remote.dto.request.SignInRequest
 import com.project.concon.model.remote.dto.request.SignUpRequest
-import com.project.concon.model.remote.dto.response.Msg
-import com.project.concon.model.remote.dto.response.ParticipatedContest
 import com.project.concon.model.remote.dto.response.Profile
-import retrofit2.Call
+import io.reactivex.rxjava3.core.Single
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface AccountService {
     /* 로그인 */
     @POST("/signin")
-    fun postSignIn(@Body signInRequest: SignInRequest): Call<Msg>
+    fun postSignIn(@Body signInRequest: SignInRequest): Single<Response<Res<Any>>>
 
     /* 회원가입 */
     @POST("/signup")
-    fun postSignUp(@Body signUpRequest: SignUpRequest): Call<Msg>
+    fun postSignUp(@Body signUpRequest: SignUpRequest): Single<Response<Res<Any>>>
 
     /* 아이디 중복체크 */
     @POST("/check")
-    fun postCheckId(@Body id: IdRequest): Call<Msg>
+    fun postCheckId(@Body id: IdRequest): Single<Response<Res<Any>>>
 
     /* 프로필 조회 */
     @GET("/profile")
-    fun getProfile(): Call<Profile>
+    fun getProfile(): Single<Response<Res<Profile>>>
 
-    /* 내가 참가한 대회 조회 */
-    @GET("/profile/contest")
-    fun getMyContest() : Call<List<ParticipatedContest>>
+    /* 캐시 충전 */
+    @PUT("/charge")
+    fun putChargeCash(@Body cash: CashRequest): Single<Response<Res<Any>>>
 }
--- app/src/main/java/com/project/concon/model/remote/dao/ContestService.kt
+++ app/src/main/java/com/project/concon/model/remote/dao/ContestService.kt
@@ -1,30 +1,34 @@
 package com.project.concon.model.remote.dao
 
-import com.project.concon.model.remote.dto.request.CashRequest
+import com.project.concon.model.remote.dto.Res
 import com.project.concon.model.remote.dto.request.ContestRequest
 import com.project.concon.model.remote.dto.request.WinnerPrizeRequest
-import com.project.concon.model.remote.dto.response.*
+import com.project.concon.model.remote.dto.response.Contest
+import com.project.concon.model.remote.dto.response.ContestDetail
+import com.project.concon.model.remote.dto.response.Participant
+import com.project.concon.model.remote.dto.response.Winner
+import io.reactivex.rxjava3.core.Single
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.*
 
 interface ContestService {
     /* 대회 조회 */
     @GET("/contest")
-    fun getContest(): Call<List<Contest>>
+    fun getContestList(): Single<Response<Res<List<Contest>>>>
 
     /* 대회 세부 내용 조회 */
     @GET("/contest/{id}")
-    fun getContestDetail(@Path("id") id: Int): Call<ContestDetail>
+    fun getContestDetail(@Path("id") id: Int): Single<Response<Res<ContestDetail>>>
 
     /* 대회 생성 */
     @POST("/contest")
-    fun postCreateContest(@Body contestRequest: ContestRequest): Call<Msg>
+    fun postCreateContest(@Body contestRequest: ContestRequest): Single<Response<Res<Any>>>
 
-    /* 대회 참여 조회 */
+    /* 대회 참여자 조회 */
     @GET("/participant/{id}")
-    fun getParticipantInfo(@Path("id") id: Int): Call<List<Participant>>
+    fun getParticipantInfo(@Path("id") id: Int): Single<Response<Res<Participant>>>
 
     /* 대회 참여 */
     @Multipart
@@ -33,22 +37,27 @@ interface ContestService {
         @Path("id") id: Int,
         @Part("content") content: RequestBody,
         @Part attachment: List<MultipartBody.Part>
-    ): Call<Msg>
+    ): Single<Response<Res<Any>>>
 
-    /* 좋아유~~ */
+    /* 좋아요 */
     @PUT("/participant/{cont}/{part}")
-    fun putLikes(
+    fun putLike(
         @Path("cont") contId: Int,
         @Path("part") partId: Int
-    ): Call<Msg>
+    ): Single<Response<Res<Any>>>
 
-    /* 캐시 충전 */
-    @PUT("/charge")
-    fun putCharge(@Body cash: CashRequest): Call<Msg>
-
+    /* 우승자 리스트 가져오기 */
     @GET("/prize/{contestid}")
-    fun getWinnerList(@Path("contestid") contestid: Int) : Call<List<Winner>>
+    fun getWinnerList(@Path("contestid") contestid: Int) : Single<Response<Res<List<Winner>>>>
 
+    /* 대회 우승자 정하기 */
     @PUT("/prize/{contestid}")
-    fun winnerPrizeSelect(@Path("contestid") contestid: Int, @Body winnerPrizeRequest: List<WinnerPrizeRequest>) : Call<Msg>
+    fun putContestWinnerSelect(
+        @Path("contestid") contestid: Int,
+        @Body winnerPrizeRequest: List<WinnerPrizeRequest>
+    ) : Single<Response<Res<Any>>>
+
+    /* 내가 참가한 대회 조회 */
+    @GET("/profile/contest")
+    fun getMyParticipatedContest() : Single<Response<Res<List<Contest>>>>
 }
