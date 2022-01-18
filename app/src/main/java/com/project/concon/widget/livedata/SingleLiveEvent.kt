317aedcf82ec4e7439db1bbc9ed8d30e5a643116
최승연 <choiseungyeon416@gmail.com>
2022-01-18 22:58:43 +0900

    rename

--- app/src/main/java/com/project/concon/widget/livedata/SingleLiveEvent.kt
+++ app/src/main/java/com/project/concon/widget/livedata/SingleLiveEvent.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import java.util.concurrent.atomic.AtomicBoolean
 
-class SingleLiveData<T> : MutableLiveData<T>() {
+class SingleLiveEvent<T> : MutableLiveData<T>() {
 
     companion object {
         private const val TAG = "SingleLiveData"
