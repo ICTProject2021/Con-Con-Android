499c20eb8a7be47a72c5a05f2e3f1fb1fc3f83f0
최승연 <choiseungyeon416@gmail.com>
2022-02-05 14:39:06 +0900

    오토로그인 기능 추가

--- app/src/main/java/com/project/concon/view/activity/IntroActivity.kt
+++ app/src/main/java/com/project/concon/view/activity/IntroActivity.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.project.concon.R
+import com.project.concon.base.BaseActivity
+import com.project.concon.widget.extension.startActivityWithFinish
+import com.project.concon.widget.utils.PreferenceUtils.token
 
-class IntroActivity : AppCompatActivity() {
+class IntroActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        verifyPermission()
+        if (token != null) startActivityWithFinish(MainActivity::class.java)
+        else verifyPermission()
     }
 
     // Permission 체크 (이미지 접근 권한)
